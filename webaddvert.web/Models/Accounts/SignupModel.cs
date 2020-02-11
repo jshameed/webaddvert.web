980f367238e4759a85f11c2ee3b93742cd29332a
jshameed <jshameed@gmail.com>
2020-02-11 12:28:01 +0400

    Initial project

---
+++ webaddvert.web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webaddvert.web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage ="Minimum length 6")]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Minimum length 6")]
+        [Compare("Password",ErrorMessage ="Password and its confirmation dont match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
